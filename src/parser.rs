7576ea5b985ea9fb7f5e52a08370a467f5db2267
thomasqmn <thumus@tutanota.com>
2023-06-26 22:14:39 +0200

    feat(parser): New parser with a partial support of all of the commands

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,21 @@
+use std::collections::VecDeque;
+
+#[derive(Clone, PartialEq, Eq)]
 pub enum CLIOptions {
-    New,
+    New, // Fait
     Create,
     Delete,
-    Add,
-    Remove,
+    Install, // Fait
+    Uninstall,
+    Run, // Fait
+
+    // Flags
     Help,
+
+    // Rest
     Unknown,
+
+    // Text
     Token(String),
 }
 
@@ -15,24 +25,26 @@ impl CLIOptions {
             CLIOptions::New => String::from("new"),
             CLIOptions::Create => String::from("create"),
             CLIOptions::Delete => String::from("delete"),
-            CLIOptions::Add => String::from("add"),
-            CLIOptions::Remove => String::from("remove"),
+            CLIOptions::Install => String::from("install"),
+            CLIOptions::Uninstall => String::from("uninstall"),
+            CLIOptions::Run => String::from("run"),
+
             CLIOptions::Help => String::from("help"),
             CLIOptions::Unknown => String::from("unknown"),
-            CLIOptions::Token(aaa) => aaa.clone(),
+            CLIOptions::Token(aaa) => aaa.to_string(),
         }
     }
 }
 
 pub struct Parser {
-    tokens: Vec<CLIOptions>,
-    args: Vec<String>,
+    pub tokens: VecDeque<CLIOptions>,
+    args: VecDeque<String>,
 }
 
 impl Parser {
-    pub fn new(args: Vec<String>) -> Self {
+    pub fn new(args: VecDeque<String>) -> Self {
         Self {
-            tokens: vec![],
+            tokens: VecDeque::new(),
             args,
         }
     }
@@ -41,42 +53,53 @@ impl Parser {
         for e in &self.tokens {
             print!("{} ", e.display());
         }
-        println!("");
+        println!();
     }
 
     pub fn parse(&mut self) {
-        let mut i = 0;
+        let mut i: usize = 0;
+        let mut next: u32 = 0;
         while i < self.args.len() {
             match self.args[i].as_str() {
                 "n" | "new" | "init" => {
-                    self.tokens.push(CLIOptions::New);
-                    i += 1;
-                    self.tokens.push(CLIOptions::Token(self.args[i].clone()))
+                    self.tokens.push_back(CLIOptions::New);
                 }
                 "c" | "create" => {
-                    self.tokens.push(CLIOptions::Create);
-                    i += 1;
-                    self.tokens.push(CLIOptions::Token(self.args[i].clone()))
+                    self.tokens.push_back(CLIOptions::Create);
+                    next += 1;
                 }
                 "d" | "delete" => {
-                    self.tokens.push(CLIOptions::Delete);
-                    i += 1;
-                    self.tokens.push(CLIOptions::Token(self.args[i].clone()))
+                    self.tokens.push_back(CLIOptions::Delete);
+                    next += 1;
                 }
-                "a" | "add" => {
-                    self.tokens.push(CLIOptions::Add);
-                    i += 1;
-                    self.tokens.push(CLIOptions::Token(self.args[i].clone()))
+                "install" | "i"| "a" | "add" => {
+                    self.tokens.push_back(CLIOptions::Install);
+                    next += 1;
                 }
-                "r" | "remove" => {
-                    self.tokens.push(CLIOptions::Remove);
-                    i += 1;
-                    self.tokens.push(CLIOptions::Token(self.args[i].clone()))
+                "uninstall" | "uni"| "rm" | "remove" => {
+                    self.tokens.push_back(CLIOptions::Uninstall);
+                    next += 1;
                 }
-                "h" | "--help" | "help" => self.tokens.push(CLIOptions::Help),
-                _ => self.tokens.push(CLIOptions::Unknown),
+                "r" | "run" | "compile" => {
+                    self.tokens.push_back(CLIOptions::Run);
+                    next += 1;
+                }
+                "--help" | "help" | "-h" => {
+                    self.tokens.push_back(CLIOptions::Help);
+                    return;
+                },
+
+                _ => {
+                    if next > 0 {
+                        self.tokens
+                            .push_back(CLIOptions::Token(self.args[i].clone()));
+                        next -= 1;
+                    } else {
+                        self.tokens.push_back(CLIOptions::Unknown)
+                    }
+                }
             };
             i += 1;
-        };
+        }
     }
 }
