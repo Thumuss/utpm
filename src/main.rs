cfe7afd2b20aef65f2cdae98f01143fc690693ed
thomasqmn <thumus@tutanota.com>
2023-06-26 22:15:46 +0200

    feat(main): Link the project with the main fn

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
+use std::collections::VecDeque;
 use std::env;
+
 use crate::parser::Parser;
+use crate::runner::Runner;
 
 pub mod parser;
+pub mod runner;
+pub mod utils;
 
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    args.remove(0);
+    let mut args: VecDeque<String> = env::args().collect();
+    args.pop_front();
     let mut parser = Parser::new(args);
     parser.parse();
     parser.display_command();
+
+    let mut runner = Runner::new(parser.tokens.clone());
+    runner.run();
 }
+
