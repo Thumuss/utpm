682ba62dea95c4a7a2c81b942704d8d5cd441fd4
thomasqmn <thumus@tutanota.com>
2023-09-07 23:50:06 +0200

    feat(build.sh): Better build

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-git clone https://github.com/ThumusLive/utpm.git &&
-cd utpm &&
-cargo install --path .
+if [ -d "utpm" ]; then
+    cd utpm && 
+    git pull origin main && 
+    cargo install --path .
+else
+    git clone https://github.com/ThumusLive/utpm.git
+    cd utpm && cargo install --path .
+fi
+
+
